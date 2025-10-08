490abd3044f59d6c214d97135043a4cbd09ef884
+ <+>
2025-10-08 12:22:35 +0300

    REACT HOOKS IMPORT EXPORT

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { image } from "../data/user";
 
 function About() {
+
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
       <img src={image} alt="I made this" />
+      <image />
     </div>
   );
 }
+
+
+export default About;
